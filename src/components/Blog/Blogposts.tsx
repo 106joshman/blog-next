da8cd8d78fb77cb022f9afc373e61f5a9eb35b27
Joshua <ejembijoshman@gmail.com>
2023-11-16 19:36:25 +0100

    fetch profile data error yet push for web testing

--- src/components/Blog/Blogposts.tsx
+++ src/components/Blog/Blogposts.tsx
@@ -7,14 +7,12 @@ import BlogCard from "./BlogCard";
 export default function Blogpost() {
   // const [articleNumber, setArticleNumber] = useState(8);
 
-
-
   return (
     <div className="container mx-auto">
       <h4 className="text-xl p-4">All blog posts</h4>
       <div className="flex flex-col sm:grid gap-2 sm:grid-cols-2 lg:grid-col-3 xl:grid-cols-4 pt-10 pb-5 border-b border-[1px_solid_rgba(230_230_230_1)]">
         {posts?.slice(0, 8).map((post, index) => (
-          <BlogCard post={post} index={index} />
+          <BlogCard key={post.id} post={post} index={index} />
         ))}
       </div>
 
