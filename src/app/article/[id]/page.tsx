97039359651ca4a6596861a97dadde701cbe226e
Joshua <ejembijoshman@gmail.com>
2024-02-29 23:23:23 +0100

    feat: ignore blogpost error

--- src/app/article/[id]/page.tsx
+++ src/app/article/[id]/page.tsx
@@ -1,12 +1,12 @@
 // "use client";
 import Image from "next/image";
 import React from "react";
-import { useGetPostQuery } from "@/redux/apiSlice";
+// import { useGetPostQuery } from "@/redux/apiSlice";
 
 export default async function ArticlePage(_id: any) {
-  const { data: post } = useGetPostQuery(_id);
+  // const { data: post } = useGetPostQuery(_id);
 
-  const { title, description, tags }: any = post;
+  // const { title, description, tags }: any = post;
 
   return (
     <>
@@ -22,9 +22,9 @@ export default async function ArticlePage(_id: any) {
           style={{ maxWidth: "100%", height: "100%" }}
         ></Image>
         <div className="p-2">
-          <h2 className="text-lg mb-3 font-bold">Title: {title}</h2>
+          <h2 className="text-lg mb-3 font-bold">Title: title</h2>
 
-          <p className="text-lg mb-3">{description}</p>
+          <p className="text-lg mb-3">description</p>
 
           <p className="mb-3">
             <span className="font-semibold mr-1">Related Tags:</span>
@@ -33,7 +33,7 @@ export default async function ArticlePage(_id: any) {
               className="bg-gray-400 rounded-full px-3 py-1"
               // style={{ color: generateRandomColor() }}
             >
-              {tags}
+              tags
             </li>
           </p>
         </div>
